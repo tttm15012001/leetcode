7fdbaaa47038aa9af0d8904304f702bbf8bb6b4b
Ryan <nam0968024068@gmail.com>
2026-02-13 23:57:49 +0700

    Exercise 3: Longest substring without repeat (Medium) - 2

--- LongestSubStringWithoutRepeatingChar.java
+++ LongestSubStringWithoutRepeatingChar.java
@@ -26,9 +26,34 @@ public class LongestSubStringWithoutRepeatingChar {
         return Math.max(maxLength, count);
     }
 
+    public int lengthOfLongestSubstring2(String s) {
+        int maxLength = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int maxRepeatIndex = 0;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char character = s.charAt(i);
+
+            if (map.containsKey(character)) {
+                maxLength = Math.max(maxLength, count);
+                // khi bị trùng, tính toán vị trí trùng lớn nhất (các kí tự trước đó coi như bỏ qua)
+                maxRepeatIndex = Math.max(maxRepeatIndex, map.get(character));
+                // chỉ đếm từ kí tự đầu tiên ngay sau kí tự trùng
+                count = i - maxRepeatIndex;
+                map.put(character, i);
+            } else {
+                map.put(character, i);
+                count++;
+            }
+        }
+
+        return Math.max(maxLength, count);
+    }
+
     public static void main(String[] args) {
         LongestSubStringWithoutRepeatingChar mainClass = new LongestSubStringWithoutRepeatingChar();
         String s = "abcdaaacbbcdebfgb";
-        System.out.println(mainClass.lengthOfLongestSubstring(s));
+        System.out.println(mainClass.lengthOfLongestSubstring2(s));
     }
 }
