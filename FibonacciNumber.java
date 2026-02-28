bfbc6b1b23844b0fcb45698276232d8a9e341938
Ryan <nam0968024068@gmail.com>
2026-02-28 09:48:23 +0700

    Exercise 509: Fibonacci number (Easy)

---
+++ FibonacciNumber.java
@@ -0,0 +1,14 @@
+public class FibonacciNumber {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        FibonacciNumber fib = new FibonacciNumber();
+
+        System.out.println(fib.fib(4));
+    }
+}
