7d06978355311ffe4138481078ec42bad7789a52
Ryan <nam0968024068@gmail.com>
2026-02-28 09:57:33 +0700

    Exercise 509: Fibonacci number (Easy) - Dynamic Programming

--- FibonacciNumber.java
+++ FibonacciNumber.java
@@ -6,6 +6,21 @@ public class FibonacciNumber {
         return fib(n - 1) + fib(n - 2);
     }
 
+    public int fib2(int n) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         FibonacciNumber fib = new FibonacciNumber();
 
